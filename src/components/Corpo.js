7ecc6993b636c39c21c2ea7fe23df19821edad32
jansenosorio <dev.jansen93@gmail.com>
2022-11-16 22:03:53 -0300

    feat: created stories component

--- src/components/Corpo.js
+++ src/components/Corpo.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import './Corpo.css'
+import Stories from './Stories'
 
 function Corpo() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="container-corpo">
+      <div className="container-column-1">
+        <Stories />
+        <div>Post</div>
+      </div>
+      <div className="container-column-2">
+        <div>SideBar</div>
+      </div>
+    </div>
+  )
 }
 
 export default Corpo
---
+++ src/components/Stories.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import './Stories.css'
+
+export default function Stories() {
+  return (
+    <div className="container-stories">
+      <div className="stories-img">
+        <div>
+          <img src="/assets/9gag.svg"></img>
+        </div>
+        <p>9gag</p>
+      </div>
+      <div className="stories-img">
+        <div>
+          <img src="/assets/9gag.svg"></img>
+        </div>
+        <p>9gag</p>
+      </div>
+      <div className="stories-img">
+        <div>
+          <img src="/assets/9gag.svg"></img>
+        </div>
+        <p>9gag</p>
+      </div>
+      <div className="stories-img">
+        <div>
+          <img src="/assets/9gag.svg"></img>
+        </div>
+        <p>9gag</p>
+      </div>
+      <div className="stories-img">
+        <div>
+          <img src="/assets/9gag.svg"></img>
+        </div>
+        <p>9gag</p>
+      </div>
+      <div className="stories-img">
+        <div>
+          <img src="/assets/9gag.svg"></img>
+        </div>
+        <p>9gag</p>
+      </div>
+      <div className="stories-img">
+        <div>
+          <img src="/assets/9gag.svg"></img>
+        </div>
+        <p>9gag</p>
+      </div>
+      <div className="stories-img">
+        <div>
+          <img src="/assets/9gag.svg"></img>
+        </div>
+        <p>9gag</p>
+      </div>
+    </div>
+  )
+}
